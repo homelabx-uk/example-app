daff1ea69f4e631be4a44dcbb1ba584c5ca8cb39
David Adamson <david.adamson2304@gmail.com>
2025-02-26 21:01:40 +0100

    add uuid to response

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
+const containerId = crypto.randomUUID();
+
 const app = new Hono();
-app.get("/", (c) => c.text("I am example app!"));
+app.get("/", (c) => c.text(`I am example app!\n${containerId}`));
 
 serve({ fetch: app.fetch, port: 3000 }, ({ port }) => {
   console.log(`server listening on http://localhost:${port}`);
