dd7def499a284d6b93a377b02fdc9ff5d9700eee
David Adamson <david.adamson2304@gmail.com>
2025-02-26 20:56:03 +0100

    add hono app

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+app.get("/", (c) => c.text("I am example app!"));
+
+serve({ fetch: app.fetch, port: 3000 }, ({ port }) => {
+  console.log(`server listening on http://localhost:${port}`);
+});
